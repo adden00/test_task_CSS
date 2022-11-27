15d97113fea663f812143258e3700740b272f7ad
adden <a.d.denusiv@mail.ru>
2022-11-27 19:26:39 +0300

    fixed string resource and splash fragment

--- app/src/main/java/com/example/testtaskcss/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/example/testtaskcss/presentation/splash/SplashFragment.kt
@@ -3,7 +3,7 @@ package com.example.testtaskcss.presentation.splash
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.coroutineScope
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.testtaskcss.R
 import kotlinx.coroutines.delay
@@ -13,7 +13,7 @@ class SplashFragment: Fragment(R.layout.fragment_splash) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        lifecycle.coroutineScope.launch {
+        viewLifecycleOwner.lifecycleScope.launch {
             delay(2000)
             findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
         }
